372d4a2b436fb2672514e5a56ccd934bcb2b1a91
Daniele Sergio <daniele.sergio@kynetics.com>
2018-12-06 10:15:03 +0100

    Configure minimum RetryDelay to 5 minutes

--- uf-client-service/src/main/java/com/kynetics/uf/android/UpdateFactoryService.java
+++ uf-client-service/src/main/java/com/kynetics/uf/android/UpdateFactoryService.java
@@ -182,7 +182,7 @@ public class UpdateFactoryService extends Service implements UpdateFactoryServic
                 final Map<String, String> finalTargetAttributes = targetAttributes;
                 ufService = UFService.builder()
                         .withClient(client)
-                        .withRetryDelayOnCommunicationError(delay)
+                        .withRetryDelayOnCommunicationError(Math.max(delay, 300_000))//todo refactor
                         .withTenant(tenant)
                         .withControllerId(controllerId)
                         .withInitialState(initialState)
