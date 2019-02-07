7d883e6eb223e7ca028dc66a6652e9511e572024
Daniele Sergio <daniele.sergio@kynetics.com>
2019-02-07 11:40:25 +0100

    Update copyright

--- uf-client-service/src/main/java/com/kynetics/uf/android/configuration/ConfigurationFileLoader.java
+++ uf-client-service/src/main/java/com/kynetics/uf/android/configuration/ConfigurationFileLoader.java
@@ -1,10 +1,12 @@
 /*
- * Copyright © 2017-2018  Kynetics  LLC
+ *
+ *  Copyright © 2017-2019  Kynetics  LLC
  *
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
  *  http://www.eclipse.org/legal/epl-v10.html
+ *
  */
 
 package com.kynetics.uf.android.configuration;
