a26fb59ed9518b2813df0d965a4fcdae77bac43c
Daniele Sergio <daniele.sergio@kynetics.com>
2019-02-12 11:32:56 +0100

    Restart service when it is updated

--- uf-client-service/src/main/java/com/kynetics/uf/android/receiver/StartServiceReceiver.java
+++ uf-client-service/src/main/java/com/kynetics/uf/android/receiver/StartServiceReceiver.java
@@ -1,10 +1,12 @@
 /*
- * Copyright © 2017-2018  Kynetics  LLC
+ *
+ *  Copyright © 2017-2019  Kynetics  LLC
  *
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
  *  http://www.eclipse.org/legal/epl-v10.html
+ *
  */
 
 package com.kynetics.uf.android.receiver;
@@ -12,19 +14,17 @@ package com.kynetics.uf.android.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.kynetics.uf.android.UpdateFactoryService;
 import com.kynetics.uf.android.apicomptibility.ApiVersion;
 
-/**
- * @author Daniele Sergio
- */
-public class BootCompletedReceiver extends BroadcastReceiver {
+public class StartServiceReceiver extends BroadcastReceiver {
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent != null && "android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+        if (intent != null && (Intent.ACTION_MY_PACKAGE_REPLACED.equals(intent.getAction())
+                ||  Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()))) {
             final Intent myIntent = new Intent(context, UpdateFactoryService.class);
             ApiVersion.fromVersionCode().startService(context, myIntent);
         }
