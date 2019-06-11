739380f2406cca090c69a72a781e3320b723b49f
Daniele Sergio <daniele.sergio@kynetics.com>
2019-06-11 10:44:06 +0200

    Revert: Migrate api module to kotlin

---
+++ uf-client-service/uf-client-service-api/src/main/java/com/kynetics/uf/android/api/UFServiceCommunicationConstants.java
@@ -0,0 +1,66 @@
+/*
+ *
+ *  Copyright © 2017-2019  Kynetics  LLC
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+
+package com.kynetics.uf.android.api;
+
+/**
+ * @author Daniele Sergio
+ */
+public class UFServiceCommunicationConstants {
+
+    public static final int MSG_CONFIGURE_SERVICE = 1;
+    /**
+     * Command to the service to register a client, receiving callbacks
+     * from the service.  The Message's replyTo field must be a Messenger of
+     * the client where callbacks should be sent.
+     */
+    public static final int MSG_REGISTER_CLIENT = 2;
+
+    /**
+     * Command to the service to unregister a client, ot stop receiving callbacks
+     * from the service.  The Message's replyTo field must be a Messenger of
+     * the client as previously given with MSG_REGISTER_CLIENT.
+     */
+    public static final int MSG_UNREGISTER_CLIENT = 3;
+
+    /**
+     * Command to service to set a new value.  This can be sent to the
+     * service to supply a new value, and will be sent by the service to
+     * any registered clients with the new value.
+     */
+    public static final int MSG_SERVICE_STATUS = 4;
+
+    public static final int MSG_AUTHORIZATION_REQUEST = 5;
+
+    public static final int MSG_AUTHORIZATION_RESPONSE = 6;
+
+    public static final int MSG_RESUME_SUSPEND_UPGRADE = 7;
+
+    public static final int MSG_SYNC_REQUEST = 8;
+
+    public static final int MSG_SERVICE_CONFIGURATION_STATUS = 9;
+
+    public static final String SERVICE_PACKAGE_NAME = "com.kynetics.uf.service";
+
+    public static final String SERVICE_ACTION = "com.kynetics.action.BIND_UF_SERVICE";
+
+    public static final String SERVICE_DATA_KEY = "DATA_KEY";
+
+    public static final String ACTION_SETTINGS = "com.kynetics.action.SETTINGS";
+
+    static final String ACTION_DOWNLOAD_AUTHORIZATION = "com.kynetics.action.DOWNLOAD_AUTHORIZATION";
+
+    static final String ACTION_RESTART_AUTHORIZATION = "com.kynetics.action.RESTART_AUTHORIZATION";
+
+    private UFServiceCommunicationConstants() {
+        throw new AssertionError();
+    }
+}
---
+++ uf-client-service/uf-client-service-api/src/main/java/com/kynetics/uf/android/api/UFServiceConfiguration.java
@@ -0,0 +1,243 @@
+/*
+ *
+ *  Copyright © 2017-2019  Kynetics  LLC
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+
+package com.kynetics.uf.android.api;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Daniele Sergio
+ */
+public class UFServiceConfiguration implements Serializable{
+
+    public static class Builder {
+        private Builder(){
+
+        }
+        public Builder withTenant(String tenant) {
+            this.tenant = tenant;
+            return this;
+        }
+
+        public Builder withControllerId(String controllerId) {
+            this.controllerId = controllerId;
+            return this;
+        }
+
+        public Builder withGetawayToken(String gatewayToken) {
+            this.gatewayToken = gatewayToken;
+            return this;
+        }
+
+        public Builder withTargetToken(String targetToken) {
+            this.targetToken = targetToken;
+            return this;
+        }
+
+        public Builder withRetryDelay(long retryDelay) {
+            this.retryDelay = retryDelay;
+            return this;
+        }
+
+        public Builder withUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public Builder withApiMode(boolean apiMode) {
+            this.apiMode = apiMode;
+            return this;
+        }
+
+        /**
+         * @deprecated  As of release 0.3.4, replaced by {@link #withEnable(boolean)}
+         */
+        @Deprecated
+        public Builder witEnable(boolean enable) {
+            return withEnable(enable);
+        }
+
+        /**
+         * @deprecated  As of release 0.3.4, replaced by {@link #withTargetAttributes(Map<String,String>)}
+         */
+        @Deprecated
+        public Builder witArgs(Map<String,String> args) {
+            return withTargetAttributes(args);
+        }
+
+        public Builder withEnable(boolean enable) {
+            this.enable = enable;
+            return this;
+        }
+
+        public Builder withTargetAttributes(Map<String,String> targetAttribute) {
+            if(targetAttribute != null && targetAttribute.size() > 0) {
+                this.targetAttributes = targetAttribute;
+            }
+            return this;
+        }
+
+        public Builder withIsUpdateFactoryServer(boolean isUpdateFactoryServer){
+            this.isUpdateFactoryServer = isUpdateFactoryServer;
+            return this;
+        }
+
+        public UFServiceConfiguration build() {
+            Objects.requireNonNull(tenant);
+            Objects.requireNonNull(controllerId);
+            Objects.requireNonNull(url);
+            if(retryDelay < 0 ){
+                throw new IllegalStateException("retryDelay must be grater than 0");
+            }
+            return new UFServiceConfiguration(tenant, controllerId, retryDelay, url,
+                    targetToken == null ? "" : targetToken,
+                    gatewayToken == null ? "" : gatewayToken,
+                    apiMode, enable, isUpdateFactoryServer,
+                    targetAttributes);
+        }
+
+        public boolean configurationIsValid(){
+            return notEmptyString(tenant)
+                    && notEmptyString(controllerId)
+                    && notEmptyString(url)
+                    && retryDelay > 0;
+        }
+
+        private boolean notEmptyString(String stringToTest){
+            return stringToTest != null && !stringToTest.isEmpty();
+        }
+        private String tenant;
+        private String controllerId;
+        private long retryDelay = 900_000;
+        private String url;
+        private boolean apiMode = true;
+        private boolean enable = true;
+        private boolean isUpdateFactoryServer = true;
+        private String targetToken;
+        private String gatewayToken;
+        private Map<String,String> targetAttributes;
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public String getTenant() {
+        return tenant;
+    }
+
+    public String getControllerId() {
+        return controllerId;
+    }
+
+    public long getRetryDelay() {
+        return retryDelay;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Boolean isApiMode() {
+        return apiMode;
+    }
+
+    public Boolean isEnable() {
+        return enable;
+    }
+
+    public String getTargetToken() {
+        return targetToken;
+    }
+
+    public String getGatewayToken() {
+        return gatewayToken;
+    }
+
+    /**
+     * @deprecated  As of release 0.3.4, replaced by {@link #getTargetAttributes()}
+     */
+    @Deprecated
+    public Map<String, String> getArgs() {
+        return getTargetAttributes();
+    }
+
+    public Map<String, String> getTargetAttributes() {
+        return targetAttributes;
+    }
+
+    public boolean isUpdateFactoryServe() {
+        return isUpdateFactoryServe;
+    }
+
+    private UFServiceConfiguration(String tenant,
+                                   String controllerId,
+                                   long retryDelay,
+                                   String url,
+                                   String targetToken,
+                                   String gatewayToken,
+                                   boolean apiMode,
+                                   boolean isEnable,
+                                   boolean isUpdateFactoryServe,
+                                   Map<String,String> targetAttributes) {
+        this.tenant = tenant;
+        this.controllerId = controllerId;
+        this.retryDelay = retryDelay;
+        this.url = url;
+        this.targetToken = targetToken;
+        this.gatewayToken = gatewayToken;
+        this.apiMode = apiMode;
+        this.enable = isEnable;
+        this.isUpdateFactoryServe = isUpdateFactoryServe;
+        this.targetAttributes = targetAttributes;
+    }
+
+    private final String tenant;
+    private final String controllerId;
+    private final long retryDelay;
+    private final String url;
+    private final String targetToken;
+    private final String gatewayToken;
+    private final boolean apiMode;
+    private final boolean enable;
+    private final Map<String,String> targetAttributes;
+    private final boolean isUpdateFactoryServe;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UFServiceConfiguration that = (UFServiceConfiguration) o;
+        return retryDelay == that.retryDelay &&
+                apiMode == that.apiMode &&
+                enable == that.enable &&
+                isUpdateFactoryServe == that.isUpdateFactoryServe &&
+                Objects.equals(tenant, that.tenant) &&
+                Objects.equals(controllerId, that.controllerId) &&
+                Objects.equals(url, that.url) &&
+                Objects.equals(targetToken, that.targetToken) &&
+                Objects.equals(gatewayToken, that.gatewayToken) &&
+                Objects.equals(targetAttributes, that.targetAttributes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tenant, controllerId, retryDelay, url, targetToken, gatewayToken, apiMode, enable, targetAttributes, isUpdateFactoryServe);
+    }
+
+    private static final long serialVersionUID = -6025361892414738765L;
+}
