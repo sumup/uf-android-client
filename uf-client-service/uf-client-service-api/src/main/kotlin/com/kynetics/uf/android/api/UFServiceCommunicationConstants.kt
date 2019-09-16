b5a5e1e62547f065e47554747b2f1c4e19e1690d
Daniele Sergio <daniele.sergio@kynetics.com>
2019-09-16 09:44:31 +0200

    refactor(Service): Convert java classes of api module to kotlin
    Signed-off-by: Daniele Sergio <daniele.sergio@kynetics.com>

---
+++ uf-client-service/uf-client-service-api/src/main/kotlin/com/kynetics/uf/android/api/UFServiceCommunicationConstants.kt
@@ -0,0 +1,53 @@
+/*
+ *
+ *  Copyright © 2017-2019  Kynetics  LLC
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+
+package com.kynetics.uf.android.api
+
+/**
+ * @author Daniele Sergio
+ */
+@Deprecated("")
+object UFServiceCommunicationConstants{
+
+    const val MSG_CONFIGURE_SERVICE = 1
+
+    const val MSG_REGISTER_CLIENT = 2
+
+    const val MSG_UNREGISTER_CLIENT = 3
+
+    const val MSG_SERVICE_STATUS = 4
+
+    const val MSG_AUTHORIZATION_REQUEST = 5
+
+    const val MSG_AUTHORIZATION_RESPONSE = 6
+
+    const val MSG_RESUME_SUSPEND_UPGRADE = 7
+
+    const val MSG_SYNC_REQUEST = 8
+
+    const val MSG_SERVICE_CONFIGURATION_STATUS = 9
+
+    const val MSG_FORCE_PING = 10
+
+    const val SERVICE_PACKAGE_NAME = "com.kynetics.uf.service"
+
+    const val SERVICE_ACTION = "com.kynetics.action.BIND_UF_SERVICE"
+
+    const val SERVICE_DATA_KEY = "DATA_KEY"
+
+    const val SERVICE_API_VERSION_KEY = "API_VERSION_KEY"
+
+    const val ACTION_SETTINGS = "com.kynetics.action.SETTINGS"
+
+    internal const val ACTION_DOWNLOAD_AUTHORIZATION = "com.kynetics.action.DOWNLOAD_AUTHORIZATION"
+
+    internal const val ACTION_RESTART_AUTHORIZATION = "com.kynetics.action.RESTART_AUTHORIZATION"
+}
---
+++ uf-client-service/uf-client-service-api/src/main/kotlin/com/kynetics/uf/android/api/UFServiceMessage.kt
@@ -0,0 +1,52 @@
+/*
+ *
+ *  Copyright © 2017-2019  Kynetics  LLC
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+
+package com.kynetics.uf.android.api
+
+import com.kynetics.uf.android.api.v1.UFServiceMessageV1
+
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.io.Serializable
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+
+/**
+ * @author Daniele Sergio
+ */
+@Deprecated(
+    "As of release 1.0.0 replaced by {@link UFServiceMessageV1}"
+)
+class UFServiceMessage(
+    val eventName: String,
+    val oldState: String,
+    val currentState: String,
+    val suspend: Suspend
+) : Serializable {
+    val dateTime: String
+
+    enum class Suspend {
+        NONE, DOWNLOAD, UPDATE
+    }
+
+    init {
+        val dateFormat = SimpleDateFormat("HH:mm:ss")
+        this.dateTime = dateFormat.format(Date())
+    }
+
+    companion object {
+        private const val serialVersionUID = -7571115123564137773L
+    }
+}
